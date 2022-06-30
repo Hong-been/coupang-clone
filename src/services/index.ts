883ca55ca1775d40befe54f7938381d7b3c94970
HongBeen <redbean-2@naver.com>
2022-06-30 17:15:36 +0900

    add: remove types.ts

--- src/services/index.ts
+++ src/services/index.ts
@@ -2,4 +2,3 @@ export { default as AuthService } from "./auth.service";
 export * from "./auth.service";
 export { default as UserService } from "./user.service";
 export * from "./user.service";
-export * from "./types";
