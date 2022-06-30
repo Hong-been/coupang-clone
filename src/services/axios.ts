0fd6ea5d3ea141133b739e21a5f6240f40112ec9
HongBeen <redbean-2@naver.com>
2022-06-30 17:14:26 +0900

    fix: axios mocule naming

--- src/services/axios.ts
+++ src/services/axios.ts
@@ -2,6 +2,6 @@ import axios from "axios";
 
 const API_HOST = process.env.NEXT_PUBLIC_API_HOST || "";
 
-export const Axios = axios.create({
+export const apiAxios = axios.create({
   baseURL: API_HOST,
 })
